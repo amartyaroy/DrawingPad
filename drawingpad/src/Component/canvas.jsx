75d1d8012d73677213fb01ee6aa9cb1a9f5f222d
Amartya Roy <amartyakumar@bitpod.io>
2020-03-29 17:59:19 +0530

    added a canvas class

---
+++ drawingpad/src/Component/canvas.jsx
@@ -0,0 +1,33 @@
+import React,{Component} from "react";
+import reactDom from "react-dom";
+import CanvasDraw from 'react-canvas-draw';
+
+class Canvas extends Component {
+    state = { 
+    color: "#ffc600",
+    width: 900,
+    height: 900,
+    brushRadius: 10,
+    lazyRadius: 12
+
+     }
+     componentDidMount() {
+        // let's change the color randomly every 2 seconds. fun!
+        window.setInterval(() => {
+          this.setState({
+            color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+          });
+        }, 2000);
+      }
+    render() { 
+        return (
+           <div>
+                <CanvasDraw
+                   
+                />
+           </div> 
+          )
+    }
+}
+ 
+export default Canvas;
--- drawingpad/src/Component/login.jsx
+++ drawingpad/src/Component/login.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Authenticator } from "@bitpod/platform-bar-shell-react";
 import { getPlatformBarConfig } from  '../Config';
+import Canvas from './canvas';
+
 let PlatformBarConfig = getPlatformBarConfig();
+let access_token;
+PlatformBarConfig["on_auth_state_change"] = function(state1) {
+  
+  if(state1!=null){
+    access_token=state1.access_token;
+  
+  }
+ };
 
-
-PlatformBarConfig["on_auth_state_change"] = function(state) {
-  console.log("state",state);
-};
 //this is the first checkinf
 class login extends Component {
   constructor(props) {
@@ -17,16 +23,45 @@ class login extends Component {
         window.React = React;
         window.ReactDOM = ReactDOM; 
     }
+    
   }
+  state={
+    isLoggedIn:false
+  }
+
+
+
+  setLoginState=(status)=>{
+    
+    console.log('setting state',status);
+    if(this.status!=null){
+     
+      this.setState({isLoggedIn:true})
+    }
+    console.log('set state',this.state.isLoggedIn);
+    
+  }
+ 
+  
+
   render() {
+    let { isLoggedIn } =this.state;
     return (
       <div className="bg bg-color=blue">
        
               <ul id="nav">
                 
                 <li><Authenticator initConfig={ PlatformBarConfig } /></li>
+                
+                
+                
               </ul>
-        
+              <Canvas/>
+           
+
+             
+              
+              
       </div>
     );
   }
